51e22dbde9ad118d1f5dbb5f8afc2b1bc68a64a7
bryanpow <ramosbusiness544@gmail.com>
2024-01-10 17:13:30 -0500

    update

--- src/server.py
+++ src/server.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+import sys
 import routes
-
+sys.path.append(os.path.dirname(__file__))
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
